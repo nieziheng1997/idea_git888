d929fdf8e4b8f28b515007ccfc4f460ed96a4872
maidi <123@qq.com>
2022-09-07 20:14:51 +0800

    1111

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("新建文件");
         System.out.println("合并分支");
         System.out.println("分支提交");
+        System.out.println("哥们我新写的提交了");
     }
 }
